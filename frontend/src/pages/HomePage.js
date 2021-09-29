3cf9ae64f4d622c0601d8fd0c222a211615ec55e
radwan <radwan.mohamed@grandmstg.com>
2021-09-29 03:46:23 -0400

    list products redux

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -3,6 +3,8 @@ import {useDispatch , useSelector} from "react-redux"
 import { Col, Row } from 'react-bootstrap';
 import Product from '../components/Product';
 import {listProducts} from "../actions/productAction";
+import Message from '../components/Message';
+import Loader from '../components/Loader';
 //import products from "../products.js";
 const HomePage = () => {
     const dispatch = useDispatch();
@@ -15,7 +17,7 @@ const HomePage = () => {
         <Fragment>
             <h1>Latest Products</h1>
             {
-                loading ? (<h1>Loading....</h1>) : error ? (<h3>{error}</h3>):(
+                loading ? (<Loader />) : error ? (<Message variant='danger'>{error}</Message>):(
                     <Row>
                     {products.map(product => (
                         <Col key={product._id} sm={12} md={6} lg={4} xl={3} >
